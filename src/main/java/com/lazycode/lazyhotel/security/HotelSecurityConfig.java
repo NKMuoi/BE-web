fc141ef82759f90a3dd05c486b725a4bd5cf5d84
DeveloperVip <130381016+DeveloperVip@users.noreply.github.com>
2025-05-15 12:28:33 +0700

    Update HotelSecurityConfig.java

--- src/main/java/com/lazycode/lazyhotel/security/HotelSecurityConfig.java
+++ src/main/java/com/lazycode/lazyhotel/security/HotelSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
 
 @Configuration
